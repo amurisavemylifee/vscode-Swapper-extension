c2049fdd674c9c3b51a6371dd2ba4d6f5fbb327b
Taras Shashurin <taras.shashurin.work@gmail.com>
2023-03-03 02:47:20 +0300

    Implemented lines swap function (Issue #1) + small package.json fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to the "swap" extension will be documented in this file.
 
-## [Unreleased]
+## [0.0.2]
+
+- Implemented lines swap function (Issue #1) + small package.json fixes
+
+## [0.0.1]
 
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -2,44 +2,65 @@ import * as vscode from "vscode";
 
 function swap() {
   const editor = vscode.window.activeTextEditor;
-  const selections = editor!.selections;
 
-  if (selections.length < 2) {
-    return vscode.window.showErrorMessage(`Too few selected points (need 2)`);
-  }
-
-  if (selections.length > 2) {
-    return vscode.window.showErrorMessage(`Too many selected points (need 2)`);
-  }
-
-  const selectionAndTextArray = selections
-    .map((selection, i) => {
+  function getSelectionAndTextArray(
+    selectionsParam: readonly vscode.Selection[]
+  ) {
+    return selectionsParam.map((selection, i) => {
       const idx = i === 0 ? 1 : 0;
       return {
-        selection: selections[idx],
+        selection: selectionsParam[idx],
         text: editor!.document.getText(selection)
       };
-    })
-    .filter(({ text }) => {
-      return text;
     });
+  }
 
-  if (selectionAndTextArray.length !== 2) {
+  if (editor!.selections.length < 2) {
+    return vscode.window.showErrorMessage(`Too few points to swap (need 2)`);
+  }
+
+  if (editor!.selections.length > 2) {
+    return vscode.window.showErrorMessage(`Too many points to swap (need 2)`);
+  }
+
+  let selectionAndTextArray = getSelectionAndTextArray(editor!.selections);
+
+  const isBothSelectionsIncludeText =
+    selectionAndTextArray.filter((el) => el.text).length === 1;
+
+  const isNoOneSelectionsIncludeText =
+    selectionAndTextArray.filter((el) => el.text).length === 0;
+
+  if (isBothSelectionsIncludeText) {
     return vscode.window.showErrorMessage("There's no selected texts to swap");
   }
 
-  return editor!.edit((builder) => {
-    selectionAndTextArray.forEach(({ selection, text }) =>
-      builder.replace(selection, text)
+  if (isNoOneSelectionsIncludeText) {
+    const firstLine = editor!.document.lineAt(
+      selectionAndTextArray[0].selection.start.line
     );
+    const secondLine = editor!.document.lineAt(
+      selectionAndTextArray[1].selection.end.line
+    );
+
+    editor!.selections = [
+      new vscode.Selection(firstLine.range.start, firstLine.range.end),
+      new vscode.Selection(secondLine.range.start, secondLine.range.end)
+    ];
+
+    selectionAndTextArray = getSelectionAndTextArray(editor!.selections);
+  }
+
+  return editor!.edit((builder) => {
+    selectionAndTextArray.forEach(({ selection, text }) => {
+      builder.replace(selection, text);
+    });
   });
 }
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand("Swapper.swap", () => {
-      swap();
-    })
+    vscode.commands.registerCommand("Swapper.swap", swap)
   );
 }
 
